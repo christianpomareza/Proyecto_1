881c734cca2c031c5e475c9f7aa6c75fe3ac4ec4
Christian <christian.poma.r@uni.pe>
2025-06-09 14:15:11 -0500

    migrando main a MVC

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
+# src/main.py
 from PyQt6.QtWidgets import QApplication
-from controller.main_controller import MainController
+import sys
+import os
+from src.controller.main_controller import MainController # Importación absoluta para el script principal
 
 if __name__ == "__main__":
-    app = QApplication([])
-    window = MainController()
-    window.show()
+
+    # Esto es un bloque para verificar y crear la carpeta 'assets'
+    if not os.path.exists("assets"):
+        os.makedirs("assets")
+        print("Crea una carpeta 'assets' y coloca allí los iconos de las aplicaciones")
+        print("Nombres esperados: playstore.png, chrome.png, settings.png, phone.png, etc.")
+
+    app = QApplication(sys.argv)
+    ventana = MainController()
+    ventana.show()
     app.exec()
